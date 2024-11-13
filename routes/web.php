0974ac6687ce03d80203d92d6abcae26e7ca7f25
RaminBgrn <ramin.bagheriian@gmail.com>
2024-11-13 20:50:39 +0330

    private channel hasbeen completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Events\PrivateEvent;
+use App\Events\testingEvent;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test' , function(){
+    event(new PrivateEvent('Hello Ramin' , 2));
+    return 'done';
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
